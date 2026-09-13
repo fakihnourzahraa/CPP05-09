7830a371855ab9ecba22250dc358df31d23b8d6f
Nour Al Zahraa Fakih <nfakih@c2r5s7.42beirut.com>
2026-09-13 17:53:15 +0300

    actually done

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nfakih <nfakih@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 13:03:17 by nour              #+#    #+#             */
-/*   Updated: 2026/09/13 17:40:26 by nfakih           ###   ########.fr       */
+/*   Updated: 2026/09/13 17:52:59 by nfakih           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,11 @@ Base *generate(void)
 	}
 	else if (num == 2)
 	{
-				std::cout <<"B generated"<<std::endl;
+		std::cout <<"B generated"<<std::endl;
 		Base *a = new B();
 		return a;
-	}		std::cout <<"C generated"<<std::endl;
+	}		
+		std::cout <<"C generated"<<std::endl;
 		Base *a = new C();
 		return a;
 }
@@ -44,28 +45,76 @@ void identify(Base *p)
 	if (a)
 	{
 		std::cout <<"Pointer is A"<<std::endl;
-		delete a;
 		return ;
 	}
 	Base *b = dynamic_cast<B*>(p);
 	if (b)
 	{
 		std::cout <<"Pointer is B"<<std::endl;
-		delete b;
 		return ;
 	}
 	Base *c = dynamic_cast<C*>(p);
 	if (c)
 	{
 		std::cout <<"Pointer is C"<<std::endl;
-		delete c;
+		return ;
+	}
+}
+
+void identify(Base &p)
+{
+	bool r = false;
+	try 
+	{
+		Base *a = dynamic_cast<A*>(&p);
+		(void) a;
+	}
+	catch (const std::exception& e)
+	{	
+		r = true;
+	}
+	if (r == false)
+	{
+		std::cout << "Pointer is A";
+		return ;
+	}
+	
+	try 
+	{
+		Base *a = dynamic_cast<B*>(&p);
+		(void) a;
+	}
+	catch (const std::exception& e)
+	{	
+		r = true;
+	}
+	if (r == false)
+	{
+		std::cout << "Pointer is B";
+		return ;
+	}
+	
+	try 
+	{
+		Base *a = dynamic_cast<C*>(&p);
+		(void) a;
+	}
+	catch (const std::exception& e)
+	{	
+		r = true;
+	}
+	if (r == false)
+	{
+		std::cout << "Pointer is C";
 		return ;
 	}
 }
 
 int main(void)
 {
-	identify(generate());
+	Base *a = generate();
+	identify(a);
+	identify(&*a);
 
     return 0;
 }
